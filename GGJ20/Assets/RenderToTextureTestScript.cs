2afe360f03fa6757ddeda342c88f94daead0ee5e
xDavidLeon <david@linceworks.com>
2020-02-01 19:11:20 +0100

    2d Paint

---
+++ GGJ20/Assets/RenderToTextureTestScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+class RenderToTextureTestScript : MonoBehaviour
+{
+    public Camera cam;
+    public RenderTexture rt = null;
+    public Texture t = null;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(new Vector3(200, 200, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+        }
+
+
+        DrawTexture();
+
+
+    }
+
+    void DrawTexture()
+    {
+        RenderTexture.active = rt;
+        GL.PushMatrix();
+        GL.LoadPixelMatrix(0, 512,256,0);
+        GL.Clear(true, true, new Color(1.0f,1.0f,1.0f,0.0f));
+        Graphics.DrawTexture(new Rect(0,0,100,100), t);
+        RenderTexture.active = null;
+        GL.PopMatrix();
+        //Graphics.DrawTexture(rect, m_renderTexture, new Rect(rect.x / Screen.width, rect.y / Screen.height, (rect.x + rect.width) / Screen.width, (rect.y + rect.height) / Screen.height), 0, 0, 0, 0);
+    }
+}
+
