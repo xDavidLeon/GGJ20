4da711f209f5a9c31efad524fd29dbc3d1a6fcd0
xDavidLeon <david@linceworks.com>
2020-02-01 17:21:14 +0100

    Parts

---
+++ GGJ20/Assets/_Game/Scripts/ComponentMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentMover : RobotComponent
+{
+    [Header("Motor")]
+    public Rigidbody rb;
+    public float power = 100.0f;
+
+    public Vector3 anchorMin;
+    public Vector3 anchorMax;
+
+    public Vector3 moveDirection = Vector3.up;
+
+    private Vector3 startConnectedAnchor;
+
+    private void Start()
+    {
+        //startConnectedAnchor = motorJoint.connectedAnchor;
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+        //motorJoint.connectedAnchor = startConnectedAnchor;
+    }
+
+    public void FixedUpdate()
+    {
+        if (rb == null) return;
+
+        Vector3 targetPosition = rb.transform.position + moveDirection * Time.deltaTime * input * power;
+        rb.MovePosition(targetPosition);
+
+        //Vector3 targetAnchor = input < 0 ? anchorMin : anchorMax;
+        //motorJoint.connectedAnchor = Vector3.MoveTowards(motorJoint.connectedAnchor, targetAnchor, Time.deltaTime * Mathf.Abs(input) * power);
+    }
+}
