4551610acc6713d5c13bfc557a8f10faef199af3
xDavidLeon <david@linceworks.com>
2020-02-01 13:08:07 +0100

    First implementation of robot components

---
+++ GGJ20/Assets/_Game/Scripts/ComponentMotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentMotor : RobotComponent
+{
+    [Header("Motor")]
+    public HingeJoint motorJoint;
+    public float power = 100.0f;
+
+    public void FixedUpdate()
+    {
+        if (motorJoint == null) return;
+
+        float targetVelocity = power * input;
+        var motor = motorJoint.motor;
+        motor.targetVelocity = targetVelocity;
+
+        motorJoint.motor = motor;
+    }
+}
---
+++ GGJ20/Assets/_Game/Scripts/RobotComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotComponent : MonoBehaviour
+{
+    [Header("Robot Component")]
+    [Range(-1.0f, 1.0f)]
+    public float input = 0.0f;
+
+    public Rigidbody chassis;
+}
