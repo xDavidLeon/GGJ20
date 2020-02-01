7241555b2b5cab0421dfdb8af4a0b03aede622fa
xDavidLeon <david@linceworks.com>
2020-02-01 18:11:05 +0100

    WIP UI

---
+++ GGJ20/Assets/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink : MonoBehaviour
+{
+    public ParticleSystem particlesLiquid;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Dot(transform.up, Vector3.up) < 0)
+        {
+            particlesLiquid.Play();
+        }
+        else particlesLiquid.Stop();
+    }
+}
