ac65ca2c91e1d83e0d64494db2c871382c9aa701
xDavidLeon <david@linceworks.com>
2020-02-01 18:54:02 +0100

    Circuit tests

---
+++ GGJ20/Assets/ControlInput.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlInput : MonoBehaviour
+{
+    [Range(-1.0f, 1.0f)]
+    public float input = 0.0f;
+}
